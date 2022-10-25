68f8d88790ca313a0a3d329f5afd76e78cb4b34a
planetjunkie <dayem.uae@proton.me>
2022-10-25 12:42:18 +0400

    just did a few things, gonna work on it at home

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./**/*{html,js}"],
+  theme: {
+    colors: {
+      'vibrantpurple': '#A838FF',
+      'bloodypink': '#FF3898',
+      'darkpurple': '#11031B',
+      'flashbang': '#FFFFFF',
+      'pitch': '#000000',
+      'linkblue': '#4260FF',
+
+    },
+    extend: {},
+  },
+  plugins: [],
+}
