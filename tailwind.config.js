2bbec70e26247809da829e4535a706f3e16be6eb
Planet Junkie <dayem.uae@proton.me>
2022-12-08 18:57:25 +0400

    major changes and redesign

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,16 +2,27 @@
 module.exports = {
   content: ["./**/*{html,js}"],
   theme: {
+    extend:{
+      backgroundImage: {
+      
+  
+      },
+    },
     colors: {
-      'vibrantpurple': '#A838FF',
-      'bloodypink': '#FF3898',
-      'darkpurple': '#11031B',
+      'primaryredish': '#FA3C5A',
+      'pinkish': '#DE35A4',
+      'pinkpurpleish': '#E648F5',
+      'purpled': '#A335DE',
+      'somewhatblue': '#823CFA',
       'flashbang': '#FFFFFF',
       'pitch': '#000000',
       'linkblue': '#4260FF',
       'gildedgold': '#F5C400',
+      'transparent': '#ffffff00',
 
     },
+
+    
     extend: {},
   },
   plugins: [],
